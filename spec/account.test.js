1fb9ad8c18f1015b9007204bfe4a81d7791cf0c4
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2022-07-28 16:13:52 +0100

    2nd test passed

--- spec/account.test.js
+++ spec/account.test.js
@@ -1,9 +1,15 @@
 const Account = require("../lib/account");
 
-describe('account class', () => {
+describe('Account class', () => {
   it('shows the balance as 0', () => {
   account = new Account()
   
   expect(account.showBalance()).toBe(0);
   })
+
+  it('shows just the headers for statement', () => {
+    account = new Account()
+    
+    expect(account.statement()).toBe("date || credit || debit || balance");
+    })
 })
