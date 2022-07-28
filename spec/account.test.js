01b22420aba5587b4fea56772a315f5595e47ece
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2022-07-28 16:38:33 +0100

    eslint configured for jest and readme updated

--- spec/account.test.js
+++ spec/account.test.js
@@ -1,47 +1,46 @@
-const Account = require("../lib/account");
+const Account = require('../lib/account')
 
 describe('Account class', () => {
   it('shows the balance as 0', () => {
-  account = new Account()
-  
-  expect(account.showBalance()).toBe(0);
+    const account = new Account()
+
+    expect(account.showBalance()).toBe(0)
   })
 
   it('shows just the headers for statement', () => {
-    account = new Account()
-    
-    expect(account.statement()).toBe("date || credit || debit || balance");
-    })
+    const account = new Account()
+
+    expect(account.statement()).toBe('date || credit || debit || balance')
+  })
 
   it('shows an empty array', () => {
+    const account = new Account()
 
-    account = new Account()
+    expect(account.showTransactions()).toEqual([])
+  })
 
-    
-    expect(account.showTransactions()).toEqual([]);
-    })
-  
   it('shows a transaction', () => {
     const mockedTransaction = {
-      getDate: "01/05/2022",
+      getDate: '01/05/2022',
       getAmount: 10
     }
-    account = new Account()
+    const account = new Account()
     account.newTransaction(mockedTransaction)
-    
+
     expect(account.showTransactions()).toEqual([{
-      getDate: "01/05/2022",
+      getDate: '01/05/2022',
       getAmount: 10
     }])
-    })
-  xit('shows a transaction', () => {
+  })
+
+  it('shows a transaction', () => {
     const mockedTransaction = {
-      getDate: "01/05/2022",
+      getDate: '01/05/2022',
       getAmount: 10
     }
-    account = new Account()
+    const account = new Account()
     account.newTransaction(mockedTransaction)
-    
-    expect(account.statement()).toBe("date || credit || debit || balance \n '01/05/2022', 10 ");
-    })
+
+    expect(account.statement()).toBe("date || credit || debit || balance \n '01/05/2022', 10 ")
+  })
 })
