1df1628786bdf64eb6b74ba3244491ff983df6cb
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2022-07-28 16:23:57 +0100

    showTransactions method added (tests 3 + 4 passed)

--- spec/account.test.js
+++ spec/account.test.js
@@ -12,4 +12,36 @@ describe('Account class', () => {
     
     expect(account.statement()).toBe("date || credit || debit || balance");
     })
+
+  it('shows an empty array', () => {
+
+    account = new Account()
+
+    
+    expect(account.showTransactions()).toEqual([]);
+    })
+  
+  it('shows a transaction', () => {
+    const mockedTransaction = {
+      getDate: "01/05/2022",
+      getAmount: 10
+    }
+    account = new Account()
+    account.newTransaction(mockedTransaction)
+    
+    expect(account.showTransactions()).toEqual([{
+      getDate: "01/05/2022",
+      getAmount: 10
+    }])
+    })
+  xit('shows a transaction', () => {
+    const mockedTransaction = {
+      getDate: "01/05/2022",
+      getAmount: 10
+    }
+    account = new Account()
+    account.newTransaction(mockedTransaction)
+    
+    expect(account.statement()).toBe("date || credit || debit || balance \n '01/05/2022', 10 ");
+    })
 })
